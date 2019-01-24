28c243b5937eb0c619d0c3ab386ffa81ce5a9581
Jingyi <jingyi.zhang@sap.com>
2019-01-24 15:19:38 +0100

    added word embedding ranking method and clean up

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import os
 import logging
 from pathlib import Path
-from tfidf_ranking import EAL
+from eal_ranking import EAL, TfidfRanking, WembRanking
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -10,17 +10,28 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S')
 logger.setLevel(logging.INFO)
 
+if os.getenv('method') in ['tfidf', 'wemb']:
+    logger.info('ranking method set to ' + os.environ['method'])
+else:
+    raise NameError("""Please set an environment variable to indicate which ranking method to use.\n
+    Your options are: method='tfidf' or 'wemb'.\n""")
 
 dir = Path(__file__).parent.parent
-# set model file to your local path
-model_file = Path.joinpath(dir, 'model', 'tfidf_model.pkl')
+# set 'model_file' to your own path
+if os.environ['method'] == 'tfidf':
+    model_file = Path.joinpath(dir, 'model', 'tfidf_model.pkl')
+    eal = TfidfRanking(model_file)
+elif os.environ['method'] == 'wemb':
+    model_file = Path.joinpath(dir, 'model', 'glove.6B.300d.txt')
+    eal = WembRanking(model_file)
 
 while True:
     sentence = input('enter sentence:')
     entity = input('enter entity: ')
-    eal = EAL(model_file)
-    aspect_predicted = eal.get_prediction(sentence, entity)
-    print('Predicted most relevant aspect is: %s' % aspect_predicted)
+    logging.info('start predicting...')
+    aspect_predicted, score = eal.get_prediction(sentence, entity)
+    logging.info('end predicting.')
+    print('Predicted most relevant aspect is: %s(score: %.4f)' % (aspect_predicted, score))
 
 
 
